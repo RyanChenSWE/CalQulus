c434c6b1aea71d5e786bb8f8a7fccb21ae266d23
Alex S <qwshao@uwaterloo.ca>
2022-10-01 22:07:29 -0400

    reformatted

--- latex-to-solution/api.py
+++ latex-to-solution/api.py
@@ -47,21 +47,19 @@ r = requests.get(query_url).json()["queryresult"]
 
 # pprint(r)
 
-#check if a pod contains id "Result"
+# check if a pod contains id "Result"
 if any(pod["id"] == "Result" for pod in r["pods"]):
 
-    #get the pod with id "Result"
+    # get the pod with id "Result"
     result_pod = next(pod for pod in r["pods"] if pod["id"] == "Result")
 
     # print all plaintexts in the pod
     for subpod in result_pod["subpods"]:
         print(subpod["plaintext"])
-
-    # print(next(pod["subpods"][0]["plaintext"] for pod in r["pods"] if pod["id"] == "Result"))
 else:
     # get results pod
     results_pod = list(filter(lambda x: x['numsubpods'] > 0 and x['subpods']
-                      [0]['plaintext'] != '', r['pods']))[0]['subpods'][0]['plaintext']
+                              [0]['plaintext'] != '', r['pods']))[0]['subpods'][0]['plaintext']
 
     if (len(results_pod) == 0):
         print("No steps shown.")
